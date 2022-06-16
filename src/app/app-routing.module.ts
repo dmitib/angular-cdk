e696ea8aa127a8f714dfa6d14b6947c76e2a15ff
Dmitrii Gushchin <dgushchinws@gmail.com>
2022-06-16 23:30:24 +0300

    add initial layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrdersPageComponent } from './orders-page/orders-page.component';
+import { UsersPageComponent } from './users-page/users-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: UsersPageComponent },
+  { path: 'orders', component: OrdersPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActionAreaComponent } from './action-area/action-area.component';
+import { MaterialModule } from './material.module';
+import { HeaderComponent } from './header/header.component';
+import { OrdersPageComponent } from './orders-page/orders-page.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { UsersPageComponent } from './users-page/users-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ActionAreaComponent,
+    UsersPageComponent,
+    OrdersPageComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
